a0ede17eed109e643c5a632f258a5abc83e5c5c7
john911120 <53751665+john911120@users.noreply.github.com>
2022-02-12 10:36:30 +0900

    Add files via upload

---
+++ 03-3 jsx/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Potato() {
+  return <h3>I Like Potato</h3>
+}
+
+function App() {
+//  return <div className="App"/>;
+  return <div>
+    <h1>Hello</h1>
+    <Potato />
+  </div>
+}
+
+export default App;
